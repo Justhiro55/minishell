1b1161d421c37f5e227b2da08c1e424f3dd03729
Hiromichi Hagiwara <hhagiwar@c1r11s1.42tokyo.jp>
2023-10-13 22:56:49 +0900

    update:pipe.c

--- src/pipe/pipe.c
+++ src/pipe/pipe.c
@@ -6,50 +6,15 @@
 /*   By: hhagiwar <hhagiwar@student.42Tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 16:50:37 by hhagiwar          #+#    #+#             */
-/*   Updated: 2023/10/13 22:50:40 by hhagiwar         ###   ########.fr       */
+/*   Updated: 2023/10/13 22:56:19 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/builtin.h"
 #include "../../includes/minishell.h"
 
-int	ft_exec(char **command, char **envp, t_info *info)
-{
-	char	**path;
-	int		i;
-	t_env	*env;
-	char	*command_path;
-
-	i = 0;
-	env = info->env;
-	while (ft_strcmp(env->key, "PATH") != 0 && i++ < 20)
-		env = env->next;
-	path = ft_split(env->value, ':');
-	i = 0;
-	while (path[i] != NULL)
-	{
-		command_path = ft_strjoin(ft_strjoin(path[i], "/"), command[0]);
-		if (access(command_path, F_OK) == 0)
-		{
-			execve(command_path, command, envp);
-			return (0);
-		}
-		free(command_path);
-		i++;
-	}
-	return (1);
-}
-
-void	set_var(t_info *info, char **argv, int argc)
-{
-	info->file_fd[0] = open(argv[1], O_RDONLY);
-	info->file_fd[1] = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	// if (info->file_fd[0] < 0 || info->file_fd[1] < 0)
-	// 	exit(EXIT_FAILURE);
-	info->pipe_num = argc - 4;
-	info->cmd1 = ft_split(argv[2], ' ');
-	info->cmd2 = ft_split(argv[3], ' ');
-}
+void	set_var(t_info *info, char **argv, int argc);
+int		ft_exec(char **command, char **envp, t_info *info);
 
 void	child_process(int fd1, int fd2, t_info info, char **envp)
 {
