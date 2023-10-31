a232037dbb23a402e5fd7c935f969604e228cd7e
Hiromichi Hagiwara <hhagiwar@c1r12s2.42tokyo.jp>
2023-10-31 16:43:45 +0900

    update:multiple_pipe&adapt_tree_structure

--- includes/parser.h
+++ includes/parser.h
@@ -6,7 +6,7 @@
 /*   By: hhagiwar <hhagiwar@student.42Tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 18:10:58 by kotainou          #+#    #+#             */
-/*   Updated: 2023/10/31 13:50:25 by hhagiwar         ###   ########.fr       */
+/*   Updated: 2023/10/31 16:26:43 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,36 @@
 # include "../func/libft/libft.h"
 # include "./minishell.h"
 
+typedef enum e_nodetype
+{
+	NODE_COMMAND,
+	NODE_PIPE,
+}						t_nodetype;
+
+typedef enum e_redirect_type
+{
+	REDIRECT_INPUT,
+	REDIRECT_OUTPUT,
+	REDIRECT_APPEND_OUTPUT,
+	REDIRECT_HEREDOC,
+}						t_redirect_type;
+
+typedef struct s_redirects
+{
+	t_redirect_type		type;
+	int					fd;
+	int					fd_file;
+	int					fd_backup;
+	char				*filename;
+	struct s_redirects	*next;
+}						t_redirects;
+
 typedef struct s_node
 {
-	// t_nodetype		type;
-	char			**data;
-	// t_redirects		*redirects;
-	struct s_node	*right;
-	struct s_node	*left;
-}					t_node;
-
+	t_nodetype			type;
+	char				**data;
+	t_redirects			*redirects;
+	struct s_node		*right;
+	struct s_node		*left;
+}						t_node;
 #endif
--- src/command/command.c
+++ src/command/command.c
@@ -6,7 +6,7 @@
 /*   By: hhagiwar <hhagiwar@student.42Tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 17:59:42 by hhagiwar          #+#    #+#             */
-/*   Updated: 2023/10/31 14:31:20 by hhagiwar         ###   ########.fr       */
+/*   Updated: 2023/10/31 16:42:05 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,28 +65,44 @@ int	ft_exec(char **command, char **envp, t_info *info)
 	return (1);
 }
 
-void	set_data(t_node *node, char *val1, char *val2)
+void	set_data(t_node *node)
 {
 	t_node	*node_child1;
 	t_node	*node_child2;
+	t_node	*node_child3;
+	t_node	*node_pipe_1;
 
 	node_child1 = (t_node *)malloc(sizeof(t_node));
 	node_child2 = (t_node *)malloc(sizeof(t_node));
+	node_child3 = (t_node *)malloc(sizeof(t_node));
 	node->data = (char **)malloc(sizeof(char *) * 2);
-	node_child1->data = (char **)malloc(sizeof(char *) * 2);
-	node_child2->data = (char **)malloc(sizeof(char *) * 2);
+	node_child1->data = (char **)malloc(sizeof(char *) * 3);
+	node_pipe_1 = (t_node *)malloc(sizeof(t_node));
+	node_child2->data = (char **)malloc(sizeof(char *) * 3);
+	node_child3->data = (char **)malloc(sizeof(char *) * 2);
 	node->data[0] = strdup("pipe");
 	node->data[1] = NULL;
-	node_child1->data[0] = strdup(val1);
-	node_child1->data[1] = NULL;
-	node_child2->data[0] = strdup(val2);
-	node_child2->data[1] = NULL;
-	node->left = node_child1;
-	node->right = node_child2;
+	node_pipe_1->data = (char **)malloc(sizeof(char *) * 2);
+	node_child1->data[0] = strdup("grep");
+	node_child1->data[1] = strdup("int");
+	node_child1->data[2] = NULL;
+	node_child2->data[0] = strdup("cat");
+	node_child2->data[1] = strdup("src/pipe/pipe_utils.c");
+	node_child2->data[2] = NULL;
+	node_child3->data[0] = strdup("wc");
+	node_child3->data[1] = NULL;
+	node_pipe_1->data[1] = NULL;
+	node->left = node_pipe_1;
+	node_pipe_1->data[0] = strdup("pipe");
+	node->right = node_child3;
+	node_pipe_1->right = node_child1;
 	node_child1->left = NULL;
+	node_pipe_1->left = node_child2;
 	node_child1->right = NULL;
 	node_child2->left = NULL;
 	node_child2->right = NULL;
+	node_child3->left = NULL;
+	node_child3->right = NULL;
 }
 
 void	child_process(t_info info, char **envp, t_node *node)
@@ -111,7 +127,10 @@ void	child_process(t_info info, char **envp, t_node *node)
 		close(pipe_fd[0]);
 		dup2(pipe_fd[1], STDOUT_FILENO);
 		close(pipe_fd[1]);
-		ft_exec(node->left->data, envp, &info);
+		if (ft_strcmp(node->left->data[0], "pipe") == 0)
+			child_process(info, envp, node->left);
+		else
+			ft_exec(node->left->data, envp, &info);
 		exit(0);
 	}
 	else
@@ -127,11 +146,11 @@ void	child_process(t_info info, char **envp, t_node *node)
 
 void	parse(char *line, t_info *info, char **envp)
 {
-	t_node *node;
+	t_node	*node;
 
 	set_token(info, line);
 	node = (t_node *)malloc(sizeof(t_node));
-	set_data(node, "ls", "wc");
+	set_data(node);
 	if (ft_strcmp(node->data[0], "pipe") == 0)
 	{
 		child_process(*info, envp, node);
