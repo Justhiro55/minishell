04d1bf988ef4c50ba00c39bf8dbc84e2b5d8f807
JustHiro55 <JustHiro271@gmail.com>
2023-11-11 12:56:49 +0900

    update:echo-option

--- src/builtin/echo_command.c
+++ src/builtin/echo_command.c
@@ -6,29 +6,43 @@
 /*   By: hhagiwar <hhagiwar@student.42Tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:54:29 by hhagiwar          #+#    #+#             */
-/*   Updated: 2023/11/02 18:53:37 by hhagiwar         ###   ########.fr       */
+/*   Updated: 2023/11/11 12:55:43 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/exec.h"
 
+int	n_option(char **token)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 2;
+	while (token[i] && token[i][0] == '-' && token[i][1] == 'n')
+	{
+		j = 2;
+		while (token[i][j] == 'n')
+			j++;
+		if (token[i][j] != '\0')
+			break ;
+		i++;
+	}
+	return (i);
+}
+
 int	command_echo(char **token)
 {
 	int	i;
-	int	n_flag;
+	int	n_set;
 
-	i = 1;
-	n_flag = 0;
 	if (token[1] == NULL)
 	{
 		ft_putstr_fd("\n", STDOUT);
 		return (0);
 	}
-	if (ft_strcmp(token[1], "-n") == 0)
-	{
-		n_flag = 1;
-		i++;
-	}
+	n_set = n_option(token);
+	i = n_set;
 	while (token[i])
 	{
 		ft_putstr_fd(token[i], STDOUT);
@@ -36,7 +50,7 @@ int	command_echo(char **token)
 			ft_putstr_fd(" ", STDOUT);
 		i++;
 	}
-	if (n_flag != 1)
+	if (n_set == 1)
 		ft_putstr_fd("\n", 1);
 	return (0);
 }
