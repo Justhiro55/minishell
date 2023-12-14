6447fc4fbd3370a2e57e78ebcf3b135ad08d1073
JustHiro55 <JustHiro271@gmail.com>
2023-12-14 19:51:24 +0900

    fix_pwd

--- src/builtin/pwd_command.c
+++ src/builtin/pwd_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pwd_command.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kotainou <kotainou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hhagiwar <hhagiwar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:55:57 by hhagiwar          #+#    #+#             */
-/*   Updated: 2023/12/12 17:37:34 by kotainou         ###   ########.fr       */
+/*   Updated: 2023/12/14 19:50:59 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,8 @@
 
 int	command_pwd(char **token)
 {
-	if (token[1] != NULL)
-	{
-		ft_putstr_fd("pwd: too many arguments\n", STDERR);
-		return (ERROR);
-	}
-	else
-	{
-		ft_putstr_fd(getcwd(NULL, 0), STDOUT);
-		ft_putstr_fd("\n", STDOUT);
-	}
+	(void)token;
+	ft_putstr_fd(getcwd(NULL, 0), STDOUT);
+	ft_putstr_fd("\n", STDOUT);
 	return (SUCCESS);
 }
