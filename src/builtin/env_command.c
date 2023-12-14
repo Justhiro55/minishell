f104534a23788dd67c2f106c9ddc7ed27b9a3e9c
JustHiro55 <JustHiro271@gmail.com>
2023-12-14 19:47:21 +0900

    fix:env

--- src/builtin/env_command.c
+++ src/builtin/env_command.c
@@ -6,7 +6,7 @@
 /*   By: hhagiwar <hhagiwar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:54:56 by hhagiwar          #+#    #+#             */
-/*   Updated: 2023/12/14 19:30:52 by hhagiwar         ###   ########.fr       */
+/*   Updated: 2023/12/14 19:47:03 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,7 @@ int	command_env(char **token, t_info info)
 	}
 	while (info.env != NULL)
 	{
-		if (info.env->value[0] != '\0')
-			printf("%s=%s\n", info.env->key, info.env->value);
+		printf("%s=%s\n", info.env->key, info.env->value);
 		info.env = info.env->next;
 	}
 	return (SUCCESS);
