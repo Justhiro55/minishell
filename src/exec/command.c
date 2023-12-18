fd3af2e279cedfb8c68683e3883f98fec4b338e0
JustHiro55 <JustHiro271@gmail.com>
2023-12-18 19:19:18 +0900

    wip

--- src/exec/command.c
+++ src/exec/command.c
@@ -6,7 +6,7 @@
 /*   By: hhagiwar <hhagiwar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 17:59:42 by hhagiwar          #+#    #+#             */
-/*   Updated: 2023/12/18 19:13:02 by hhagiwar         ###   ########.fr       */
+/*   Updated: 2023/12/18 19:18:54 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,28 +17,26 @@ void	free_redirects(t_redirects *redirects);
 void	free_node(t_node *node);
 void	free_info(t_info *info);
 
-int 	exec_left_node(t_info *info, char **envp, t_node *node, int *pipefd)
+int	exec_left_node(t_info *info, char **envp, t_node *node, int *pipefd)
 {
 	int	status;
 
 	status = 0;
 	close(pipefd[PIPE_READ]);
-	if (ft_dup2(pipefd[PIPE_WRITE], STDOUT_FILENO) == -1)
-		exit(1);
+	ft_dup2(pipefd[PIPE_WRITE], STDOUT_FILENO);
 	close(pipefd[PIPE_WRITE]);
 	if (node != NULL)
 		status = child_process(info, envp, node);
 	exit(status);
 }
 
-int 	exec_right_node(t_info *info, char **envp, t_node *node, int *pipefd)
+int	exec_right_node(t_info *info, char **envp, t_node *node, int *pipefd)
 {
 	int	status;
 
 	status = 0;
 	close(pipefd[PIPE_WRITE]);
-	if (ft_dup2(pipefd[PIPE_READ], STDIN_FILENO) == -1)
-		exit(1);
+	ft_dup2(pipefd[PIPE_READ], STDIN_FILENO);
 	close(pipefd[PIPE_READ]);
 	if (node != NULL)
 		status = child_process(info, envp, node);
