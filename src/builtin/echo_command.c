979cded465ad8c08e775883d0cf45f388c1ff336
JustHiro55 <JustHiro271@gmail.com>
2023-12-18 15:15:05 +0900

    fix echo

--- src/builtin/echo_command.c
+++ src/builtin/echo_command.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo_command.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kotainou <kotainou@student.42.fr>          +#+  +:+       +#+        */
+/*   By: hhagiwar <hhagiwar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:54:29 by hhagiwar          #+#    #+#             */
-/*   Updated: 2023/12/12 17:37:16 by kotainou         ###   ########.fr       */
+/*   Updated: 2023/12/18 14:20:47 by hhagiwar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,19 @@ int	check_arg(char **str)
 {
 	int	i;
 	int	j;
+	int	flag;
 
 	i = 0;
 	j = 0;
+	flag = 0;
 	while (str[i] != NULL)
 	{
+		printf("test:%c\n", str[i][0]);
+		if (str[i][0] == '\"' || str[i][0] == '\'')
+			flag = 1;
 		while (str[i][j] != '\0')
 		{
-			if (is_metacharacter(str[i][j]))
+			if (is_metacharacter(str[i][j]) && flag == 0)
 			{
 				ft_putstr_fd("syntax error near unexpected token \'", STDOUT);
 				ft_putchar_fd(str[i][j], STDOUT);
@@ -58,6 +63,7 @@ int	check_arg(char **str)
 			}
 			j++;
 		}
+		flag = 0;
 		j = 0;
 		i++;
 	}
@@ -69,6 +75,7 @@ int	command_echo(char **token)
 	int	i;
 	int	n_set;
 
+	printf("%s\n", token[1]);
 	if (token[1] == NULL)
 	{
 		ft_putstr_fd("\n", STDOUT);
